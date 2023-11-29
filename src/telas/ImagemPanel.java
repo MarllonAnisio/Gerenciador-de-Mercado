0fa3cc2f5212cda2a2a83ea9bfd6821b0c3fcfb0
Edenilson <81166429+Edenilson-Silva-Souza@users.noreply.github.com>
2023-11-29 08:29:07 -0300

    Adicionado interface gráficas das telas:(Acessar conta e criar nova conta do ADM

---
+++ src/telas/ImagemPanel.java
@@ -0,0 +1,29 @@
+package telas;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class ImagemPanel extends JPanel{
+	private BufferedImage image;
+	
+	public ImagemPanel(String imagePath) {
+		try {
+			image = ImageIO.read(new File(imagePath));
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if(image != null) {
+			g.drawImage(image, 0,0, this);
+		}
+	}
+}
---
+++ src/telas/Programa.java
@@ -0,0 +1,9 @@
+package telas;
+
+
+public class Programa {
+	public static void main(String[] args) {
+		//new TelaLogin();
+		//new TelaNovaContaADM();
+	}
+}
