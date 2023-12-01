a53d67b76bc199b39e24f81367403f0d604895da
Edenilson <81166429+Edenilson-Silva-Souza@users.noreply.github.com>
2023-12-01 13:17:30 -0300

    Nova tela criada(Adicionar novo produto fornecedor)

--- src/telas/Programa.java
+++ src/telas/Programa.java
@@ -4,7 +4,8 @@ public class Programa {
 	public static void main(String[] args) {
 		//new TelaLogin();
 		//new TelaNovaContaADM();
-		new TelaNovoCliente();
-		new TelaNovoFornecedor();
+		//new TelaNovoCliente();
+		//new TelaNovoFornecedor();
+		new TelaNovoProdutoMercado();
 	}
 }
